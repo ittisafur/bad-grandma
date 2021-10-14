a29d882d562204867cf0f5dc9e15c073601b0a02
ittisafur <ittisafur@gmail.com>
2021-10-14 07:03:49 +0600

    second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,15 @@ module.exports = {
       colors: {
         primaryBg: "#505050",
         secondaryBg: "#ffe45e",
+        tertiaryBg: "#363636",
         primaryText: "#ff6392",
         secondaryText: "#af9c70",
+        tertiaryText: "#feb33a",
       },
       fontSize: {
         40: "2.5rem",
         80: "5rem",
+        90: "5.625rem",
       },
       backgroundImage: {
         banner: "url('/assets/images/banner.png')",
